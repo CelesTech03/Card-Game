5522d9d3f0032a34b3cedd1bdd4f7f1aecd3f5cf
CelesTech03 <celeste.urena@stu.bmcc.cuny.edu>
2022-11-07 16:23:51 -0500

    Added new game button and ability to shuffle cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import './App.css';
+import './Card.css';
+import { useState } from 'react';
 import Card from './Card'
 
+const cardValues = ["A", "B", "C", "D"];
+
 function App() {
-    const cardValues = ["Card1", "Card2", "Card3", "Card4"];
+  // Sets state
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  const shuffleCards = () => {
+    // Doubles cardValues array and then sorts and maps them randomly
+    const shuffledCards = [...cardValues, ...cardValues]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+
+    // Shuffles cards and resets turns
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+
   return (
     <div className="App">
-      {cardValues.map((item,idx)=>(<Card key={idx} cardText={item}/>))}
+      <h1>Memory Game</h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map(card => (
+          <div className="card" key={card.id}>
+            <div>
+            {cardValues.map((item,idx)=>(<Card key={idx} cardText={item}/>))}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
