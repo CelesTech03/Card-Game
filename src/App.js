ede5ecd5cf4fbdbe7c6c9c9128802abac76819a4
CelesTech03 <celeste.urena@stu.bmcc.cuny.edu>
2022-11-07 15:23:21 -0500

    Added base template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
+import Card from './Card'
 
 function App() {
+    const cardValues = ["Card1", "Card2", "Card3", "Card4"];
   return (
     <div className="App">
-        <p>
-          Hello World!
-        </p>
+      {cardValues.map((item,idx)=>(<Card key={idx} cardText={item}/>))}
     </div>
   );
 }
